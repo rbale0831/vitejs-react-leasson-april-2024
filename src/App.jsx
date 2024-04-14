ddafa75476115dd9fac7be324396acb4d6ca8630
rbalecfc <rohit.bale@cloudfirstgroup.com>
2024-04-15 03:42:27 +0530

    code uisng useEffect

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import './App.css'
+import HomePage from './pages/HomePage'
+import MainLayout from './layouts/MainLayout'
+import JobsPage from './pages/JobsPage'
+import JobPage from './pages/JobPage'
+import NotFound from './pages/NotFound'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} />
+      <Route path='*' element={<NotFound />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />
+}
+
+export default App
