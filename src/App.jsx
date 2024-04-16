e9e73d95d062c501c9be3ea4b68af739b693135f
rbalecfc <rohit.bale@cloudfirstgroup.com>
2024-04-16 14:58:40 +0530

    fetch api using useLoaderData and using useParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,62 @@
-import React from 'react'
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
-import './App.css'
-import HomePage from './pages/HomePage'
-import MainLayout from './layouts/MainLayout'
-import JobsPage from './pages/JobsPage'
-import JobPage from './pages/JobPage'
-import NotFound from './pages/NotFound'
+import React from 'react';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import JobPage, { jobLoader } from './pages/JobPage';
+import NotFound from './pages/NotFound';
+import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} />
-      <Route path='*' element={<NotFound />} />
-    </Route>
-  )
-);
 
 const App = () => {
-  return <RouterProvider router={router} />
-}
+  // Add New Job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
 
-export default App
+  // Delete Job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
+  }
+
+  // Update Job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={ <MainLayout /> }>
+        <Route index element={ <HomePage /> } />
+        <Route path='/jobs' element={ <JobsPage /> } />
+        <Route path='/add-job' element={ <AddJobPage addJobSubmit={ addJob } /> } />
+        <Route path='/edit-job/:id' element={ <EditJobPage updateJobSubmit={ updateJob } /> } loader={ jobLoader } />
+        <Route path='/jobs/:id' element={ <JobPage deleteJob={ deleteJob } /> } loader={ jobLoader } />
+        <Route path='*' element={ <NotFound /> } />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={ router } />
+};
+
+export default App;
